6b5501c3f0eff6c686d1740928e425fb62f2b27a
Sterling Smith <sterling12311@gmail.com>
2023-10-16 08:26:42 -0600

    added html file

---
+++ 2023-10-06-javascript/client.js
@@ -0,0 +1,6 @@
+// input field variable
+let userInput = document.querySelector("#input-box");
+// submit button variable
+const submitButton = document.querySelector(#"submit-button");
+// output paragraph
+const displayUserInput = document.querySelector(#"display-input");
