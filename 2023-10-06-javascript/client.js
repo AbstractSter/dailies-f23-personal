a55f0492bf082f936d87aa96cd80283150487dc7
Sterling Smith <sterling12311@gmail.com>
2023-10-16 08:29:52 -0600

    outstanding changes

--- 2023-10-06-javascript/client.js
+++ 2023-10-06-javascript/client.js
@@ -1,6 +1,41 @@
 // input field variable
 let userInput = document.querySelector("#input-box");
 // submit button variable
-const submitButton = document.querySelector(#"submit-button");
+const submitButton = document.querySelector("#submit-button");
 // output paragraph
-const displayUserInput = document.querySelector(#"display-input");
+const displayUserInput = document.querySelector("#display-input");
+
+submitButton.addEventListener("click", function () {
+    // console.log(userInput.value);
+    displayUserInput.textContent = "";
+    
+  });
+  
+  // ------------- COUNTER -------------- //
+  
+  // create counter variable that can be shown on the page and added to to or subtracted from
+  let count = 0;
+  const displayCount = document.querySelector("#display-input");
+  const increaseButton = document.querySelector("#increase-count");
+  const decreaseButton = document.querySelector("#decrease-count");
+// create a function that can add from the variable by 1
+function increaseCount () {
+  // change the count variable
+  count++;
+  // update the DOM
+  document.querySelector("#count").innerHTML = count;
+}
+// create a function that can subtract from the variable by 1
+function decreaseCount () {
+  count--;
+  document.querySelector("#count").innerHTML = count;
+}
+
+increaseButton.addEventListener("click", function () {
+    increaseCount();
+})
+
+decreaseButton.addEventListener("click", function () {
+  decreaseCount();
+})
+
